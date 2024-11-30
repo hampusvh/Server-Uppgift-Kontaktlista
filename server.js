54a287c30bf8b6a7009d68d79571198859b4c929
hampusvh <hampus.vh@gmail.com>
2024-11-30 16:18:15 +0100

    set up mongodb connection and fixed some issues

--- server.js
+++ server.js
@@ -16,5 +16,5 @@ app.get('/', (req, res) => {
     res.send('Hej och välkommen');
 });
 
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT || 5001; 
 app.listen(PORT, () => console.log(`Servern körs på port ${PORT}`));

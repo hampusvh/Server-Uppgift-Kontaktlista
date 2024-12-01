3fc47b9b467b0ae2d3354b77e1c0778bd823b40d
hampusvh <hampus.vh@gmail.com>
2024-12-01 22:08:08 +0100

    added structural change to server.js

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ connectDB();
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+
 
 app.use('/api/auth', require('./routes/authRoutes'));
-
-app.use(express.json());
 
 app.use(express.static("public"));
 
@@ -23,5 +23,5 @@ app.get('/', (req, res) => {
     res.send('Hej och välkommen');
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001; 
 app.listen(PORT, () => console.log(`Servern körs på port ${PORT}`));
